ce15a77a2cd499bd5732e1302071dc6e66437897
Danirromero <43442039+Danirromero@users.noreply.github.com>
2020-11-09 17:26:23 +0100

    Update install_lamp.sh

--- install_lamp.sh
+++ install_lamp.sh
@@ -22,3 +22,14 @@ apt install debconf-utils -y
 DB_ROOT_PASSWORD=root
 debconf-set-selections <<< "mysql-server mysql-server/root_password $DB_ROOT_PASSWORD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DB_ROOT_PASSWORD"
+
+# Instalamos PHPMyAdmin
+APP_PASS="your-app-pwd"
+ROOT_PASS="your-admin-db-pwd"
+APP_DB_PASS="your-app-db-pwd"
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $APP_PASS" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password $DB_ROOT_PASSWORD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $DB_ROOT_PASSWORD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
+apt install -y phpmyadmin php-mbstring php-zip php-gd php-json php-curl
